564744075386472dd19481e0c1053a8302f728eb
James Mitchell <james@taylormitchell.co.uk>
2022-01-27 22:20:40 +0000

    wip - layout

--- README.md
+++ README.md
@@ -12,3 +12,24 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
  * `cdk deploy`      deploy this stack to your default AWS account/region
  * `cdk diff`        compare deployed stack with current state
  * `cdk synth`       emits the synthesized CloudFormation template
+
+
+### Required.
+
+Set source ip.
+
+```zsh
+
+export SOURCE_IP="..."
+
+```
+
+### Architecture - Basic.
+
+fe -> s3 -> sns -> lambda -> sqs -> lambda -> // -> destination.
+
+capture csv/xls -> store -> notify -> read lines, push onto queue -> queue -> pop from queue, send to dest
+
+### Architecture - Extended.
+
+Possibly add 'Progress' to from 'Pop/Sender' to Frontend.
--- lib/bulk-loader-stack.ts
+++ lib/bulk-loader-stack.ts
@@ -1,10 +1,25 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import {
+  Stack,
+  StackProps,
+  aws_s3 as s3,
+  aws_iam as iam,
+  aws_sns as sns,
+  aws_s3_notifications as s3n,
+  aws_lambda as lambda,
+  aws_sns_subscriptions as subscriptions,
+} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as path from 'path';
 
 export class BulkLoaderStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const { SOURCE_IP } = process.env;
+
+    /**
+     * TODO: Validate SOURCE_IP
+     */
 
     // The code that defines your stack goes here
 
@@ -12,5 +27,81 @@ export class BulkLoaderStack extends Stack {
     // const queue = new sqs.Queue(this, 'BulkLoaderQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    /**
+     * Frontend.
+     * TODO: Do frontend last/later, can upload to s3 from cli for now.
+     */
+
+    /**
+     * S3 data store bucket.
+     * TODO: Add encryption to datastore bucket.
+     */
+    const dataStoreBucket = new s3.Bucket(this, 'DataStoreBucket', {
+      enforceSSL: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
+
+    /**
+     * TODO: Restrict put to Frontend.
+     * TODO: Restrict get to sns (if required)
+     */
+    const dataStoreBucketPolicy = new iam.PolicyStatement({
+      actions: [
+        's3:GetObject',
+        's3:PutObject',
+      ],
+      resources: [
+        dataStoreBucket.arnForObjects('*'),
+      ],
+      principals: [
+        new iam.AnyPrincipal()
+      ],
+    });
+    /**
+     * TEMP: Restrict to ip for testing.
+     */
+    dataStoreBucketPolicy.addCondition('IpAddress', {
+      'aws:SourceIp': [SOURCE_IP],
+    });
+
+    /**
+     * Validate Data Source Bucket Resource Policy.
+     */
+    const dataStoreBucketResourcePolicy = dataStoreBucket.addToResourcePolicy(dataStoreBucketPolicy);
+    if (!dataStoreBucketResourcePolicy.statementAdded) {
+      console.log(`Failed to add dataStoreBucketResourcePolicy to ${dataStoreBucket.bucketName}`);
+    }
+
+    /**
+     * SNS
+     */
+    const dataSourceAddedNotification = new sns.Topic(this, 'DataSourceAdded');
+    dataStoreBucket.addEventNotification(
+        s3.EventType.OBJECT_CREATED,
+        new s3n.SnsDestination(dataSourceAddedNotification),
+    );
+
+    /**
+     * Lambda - Reader.
+     * Reads csv/xls, validates, adds to queue.
+     */
+    const readerLambda = new lambda.Function(this, 'Reader', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, 'Reader'))
+    });
+
+    const readerLambdaSubscription = new subscriptions.LambdaSubscription(readerLambda)
+    dataSourceAddedNotification.addSubscription(readerLambdaSubscription);
+
+    /**
+     * SQS
+     * FIFO.
+     */
+
+    /**
+     * Lambda - Sender.
+     */
   }
 }
