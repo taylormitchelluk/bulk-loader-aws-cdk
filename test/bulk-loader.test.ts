7fb47ecb21773208a4528dfb0a29f1707432a1e2
James Mitchell <james@taylormitchell.co.uk>
2022-01-27 20:37:33 +0000

    Initial commit

---
+++ test/bulk-loader.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BulkLoader from '../lib/bulk-loader-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bulk-loader-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BulkLoader.BulkLoaderStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
